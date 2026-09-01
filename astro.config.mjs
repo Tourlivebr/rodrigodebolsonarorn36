fddd08bfd9e276ae567ce4b2ec9b1b479e3a1625
FilippiArruda <tourlivebr@gmail.com>
2026-09-01 15:45:09 -0300

    atttt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rodrigodebolsonaro36.com.br',
+  site: 'https://www.rodrigodebolsonarorn36.com.br',
+  trailingSlash: 'always',
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   output: 'static',
 });
